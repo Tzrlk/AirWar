849f8d9634d3b3227c991901a7b47d32452e9323
default_git_name <default_git_email>
2012-08-16 11:31:20 +0000

    Adding the default Android project structure generated by AIDE.

---
+++ mobile/src/nz/co/aetheric/airwar/MainActivity.java
@@ -0,0 +1,17 @@
+package nz.co.aetheric.airwar;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
