76ef9e0613110d151e627a61c8d58148f2cf4c54
Tzrlk <peterc@aetheric.co.nz>
2012-09-05 14:46:38 +1200

    Upgraded the Card classes a bit (Comparable, etc.)

--- base/src/main/java/nz/co/aetheric/airwar/Card.java
+++ base/src/main/java/nz/co/aetheric/airwar/Card.java
@@ -3,7 +3,7 @@ package nz.co.aetheric.airwar;
 /**
  * This class represents a single card in a 42 card deck.
  */
-public class Card {
+public class Card implements Comparable<Card> {
     private final Face face;
     private final Suit suit;
 
@@ -16,10 +16,15 @@ public class Card {
         this.face = Face.valueOf(face);
         this.suit = Suit.valueOf(suit);
     }
+	
+	@Override
+	public int compareTo(Card other) {
+		return face.compareTo(other.face);
+	}
 
-    public enum Face {
+	//todo: split this into a separate file
+    public enum Face implements Comparable<Face> {
         ACE(1),
-        ONE(1),
         TWO(2),
         THREE(3),
         FOUR(4),
@@ -43,6 +48,7 @@ public class Card {
             return value;
         }
 
+		//todo: optimise with switch-case
         public static Face valueOf(int value) {
             for (Face face : Face.values()) {
                 if (face.value == value) {
@@ -52,8 +58,15 @@ public class Card {
 
             return null;
         }
+		
+		@Override
+		public int compareTo(Face other) {
+			//todo: check this is implemented correctly
+			return value - other.value;
+		}
     }
 
+	//todo: split this into a separate file
     public enum Suit {
         SPADES(1),
         HEARTS(2),
@@ -71,13 +84,13 @@ public class Card {
         }
 
         public static Suit valueOf(int value) {
-            for (Suit suit : Suit.values()) {
-                if (suit.value == value) {
-                    return suit;
-                }
-            }
-
-            return null;
+            switch(value) {
+				case 1: return Suit.SPADES;
+				case 2: return Suit.HEARTS;
+				case 3: return Suit.CLUBS;
+				case 4: return Suit.DIAMONDS;
+				default: return null;
+			}
         }
     }
 }
--- base/src/main/java/nz/co/aetheric/airwar/CardDeck.java
+++ base/src/main/java/nz/co/aetheric/airwar/CardDeck.java
@@ -4,26 +4,42 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class CardDeck {
-    private List<Card> deck;
+    private LinkedList<Card> deck;
+	private ArrayList<Card> active;
+	private List<Card> discard;
 
     public CardDeck() {
-        initialiseDeck();
-    }
-
-    public void shuffle() {
-
-    }
-
-    public Card draw() {
-        return null;
-    }
-
-    private void initialiseDeck() {
-        deck = new LinkedList<Card>();
+        this.deck = new LinkedList<Card>();
+		this.active = new ArrayList<Card>(5);
+		this.discard = new LinkedList<Card>();
+		
         for (Card.Suit suit : Card.Suit.values()) {
             for (Card.Face face : Card.Face.values()) {
                 deck.add(new Card(face, suit));
             }
         }
+    }
+	
+	public boolean draw() {
+		for (Card card : active) {
+			deck.addFirst(card);
+		}
+		
+		active.clear();
+		
+		for (int i = 0; i < 5; i++) {
+			Card card = deck.removeLast();
+			if (card == null) {
+				return false;
+			}
+			
+			active.add(card);
+		}
+		
+		return true;
+	}
+
+    public void shuffle() {
+        Collections.shuffle(deck);
     }
 }
